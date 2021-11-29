6355cf6f8b3ee96dfc45d2aa5e1f6f0d75ba6a19
cielpy <beijiu572@gmail.com>
2021-11-30 01:15:34 +0800

    add iplist format 2

--- clean_ips.sh
+++ clean_ips.sh
@@ -2,5 +2,7 @@
 
 latest_ips=$(tail -10 /usr/local/openresty/nginx/conf/ip_white.conf)
 echo "$latest_ips" > /usr/local/openresty/nginx/conf/ip_white.conf
+latest_ips2=$(tail -10 /usr/local/openresty/nginx/conf/ip_white2.conf)
+echo "$latest_ips2" > /usr/local/openresty/nginx/conf/ip_white2.conf
 
 /usr/bin/openresty -s reload
--- ngx_add.sh
+++ ngx_add.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 ngx_conf=/usr/local/openresty/nginx/conf/ip_white.conf
+ngx_conf2=/usr/local/openresty/nginx/conf/ip_white2.conf
 result=`cat $ngx_conf | grep $1`
 if [ -z "$result" ]; then
     echo "$1 1;" >> $ngx_conf
+    echo "$1" >> $ngx_conf2
     /usr/bin/openresty -s reload
 else
     exit 0
